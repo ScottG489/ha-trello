6738c6d22029029c3b7f757ae2e1841ea7a5f242
Scott Giminiani <scottg489@gmail.com>
2023-06-05 19:26:08 -0400

    Change name of coordinator
    This doesn't seem to be used for anything other than logging purposes
    but seems better to be consistent.

--- custom_components/trello_beta/coordinator.py
+++ custom_components/trello_beta/coordinator.py
@@ -24,7 +24,7 @@ class TrelloDataUpdateCoordinator(DataUpdateCoordinator[dict[str, int]]):
         super().__init__(
             hass=hass,
             logger=LOGGER,
-            name="trello",
+            name="trello_beta",
             update_interval=timedelta(seconds=15),
         )
         self.client = trello_client
